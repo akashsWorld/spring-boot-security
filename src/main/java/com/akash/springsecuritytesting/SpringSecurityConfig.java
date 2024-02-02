e47ce01d9e0f94d481081f98125a38e5cbe95f94
akashsWorld <akashbiswas.271999@gmail.com>
2024-02-02 18:18:01 +0530

    Update the httpsecurity to now authorize all the request Methods getting from server.

--- src/main/java/com/akash/springsecuritytesting/SpringSecurityConfig.java
+++ src/main/java/com/akash/springsecuritytesting/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.akash.springsecuritytesting;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -12,7 +13,11 @@ public class SpringSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.ignoringRequestMatchers("/api/**"));
+//       In the previous configuration the basic authentication only authenticate the GET request, now it will authenticate all request methods.
+        httpSecurity.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                authorizationManagerRequestMatcherRegistry.anyRequest().authenticated());
+        httpSecurity.httpBasic(Customizer.withDefaults());
+                httpSecurity.csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.ignoringRequestMatchers("/api/**"));
         return httpSecurity.build();
     }
 }
