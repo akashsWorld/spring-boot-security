0c8ecc7a2c16508e69450b2b80a9c4d456bff3f1
akashsWorld <akashbiswas.271999@gmail.com>
2024-02-02 16:32:06 +0530

    Project files initialised.

---
+++ src/test/java/com/akash/springsecuritytesting/SpringSecurityTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.springsecuritytesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
