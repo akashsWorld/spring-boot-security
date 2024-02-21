abfa043b9df0d856d20d7203a4f1965526f4e1fb
akashsWorld <akashbiswas.271999@gmail.com>
2024-02-22 01:23:42 +0530

    Authorization server setup implemented but not done the configurations for database.

---
+++ O-auth-authorization-spring/o-auth-authorization-server/src/main/java/com/akash/auth_server/AuthServerMain.java
@@ -0,0 +1,11 @@
+package com.akash.auth_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerMain {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServerMain.class,args);
+    }
+}
